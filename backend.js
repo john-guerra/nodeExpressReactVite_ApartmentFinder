1c6863817bb9b2bbc31e1ed4fd58a145254b076e
John Alexis Guerra Gómez <john.guerra@gmail.com>
2025-11-20 10:39:39 -0800

    Adding basic auth backend

--- backend.js
+++ backend.js
@@ -7,6 +7,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 import listingsRouter from "./routes/Listings.js";
+import authRouter from "./routes/Auth.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -16,6 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/", express.static("./frontend/dist"));
 app.use("/api", listingsRouter);
+app.use("/api/auth", authRouter);
 
 app.get("*splat", function (req, res) {
   res.sendFile("index.html", {
